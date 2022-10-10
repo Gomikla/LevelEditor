02e1065472d276a065d16686c6ad11ab64ddd1ad
Gomikla <77101211+Gomikla@users.noreply.github.com>
2022-10-10 15:57:27 +0200

    Implement dynamicly changing grid
    Add buttons and codebehind needed to generate grid

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LevelEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void initGrid()
+        {
+            int width = int.Parse(WidthCounter.Text);
+            int heigth = int.Parse(HeightCounter.Text);
+
+            // Create the Grid
+
+            Grid DynamicGrid = new Grid();
+
+            DynamicGrid.Width = 400;
+
+            DynamicGrid.HorizontalAlignment = HorizontalAlignment.Center;
+
+            DynamicGrid.VerticalAlignment = VerticalAlignment.Center;
+
+            DynamicGrid.ShowGridLines = true;
+
+
+            for(int i = 0; i < width; i++)
+            {
+                ColumnDefinition gridCol = new ColumnDefinition();
+                DynamicGrid.ColumnDefinitions.Add(gridCol);
+
+
+            }
+
+            for (int i = 0; i < heigth; i++)
+            {
+                RowDefinition gridRow = new RowDefinition();
+                gridRow.Height = new GridLength(45);
+                DynamicGrid.RowDefinitions.Add(gridRow);
+            }
+
+
+            RootWindow.Content = DynamicGrid;
+
+
+
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            initGrid();
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace LevelEditor.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+      
+    }
+}
